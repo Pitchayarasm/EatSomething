95021065fcd12b763b29149aef91a5610895ee5b
Thanyah Kung <pitchayarasm.kunghae@hotmail.com>
2019-01-28 19:07:38 -0800

    try to connect

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var mongoose = require("mongoose");
 var PORT = process.env.PORT || 3000;
 var app = express();
 var logger = require("morgan");
+var mongo = require("mongodb").MongoClient
+var dataURL = process.env.MONGOLAB_URI
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -19,8 +21,11 @@ routes(app);
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://heroku_lcknsgjl:b1ia2smi79farnukuucrsnsi71@ds141633.mlab.com:41633/heroku_lcknsgjl");
-var db = mongoose.connection;
+// mongoose.connect("mongodb://heroku_lcknsgjl:b1ia2smi79farnukuucrsnsi71@ds141633.mlab.com:41633/heroku_lcknsgjl");
+// var db = mongoose.connection;
+mongo.connect(dataURL, function(err, db) {
+    if (err) throw err;
+});
 
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
